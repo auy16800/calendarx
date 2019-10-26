a4f5e51b67218575fe4e3c964cc5dc1f02bbd207
wuy <wuyuewyy@outlook.com>
2019-10-26 14:38:08 +0800

    放开了一个斐波那契数列

--- calendarx_parent/calendarx_backend/src/test/java/TestFB.java
+++ calendarx_parent/calendarx_backend/src/test/java/TestFB.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class TestFB {
 
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("输出斐波那契数列第n个数（注意n是从0开始的）：");
-//        int n = sc.nextInt();
-//        if(n<2) {
-//            System.out.println(n);
-//        }
-//        else {
-//            //利用单循环来实现斐波那契数列
-//            int f1=0;
-//            int f2=1;
-//            int f3=0;
-//            for(int i=1;i<n;i++) {
-//                f3=f1+f2;
-//                f1=f2;
-//                f2=f3;
-//            }
-//            System.out.println("F（n）="+f3);
-//        }
-//    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输出斐波那契数列第n个数（注意n是从0开始的）：");
+        int n = sc.nextInt();
+        if(n<2) {
+            System.out.println(n);
+        }
+        else {
+            //利用单循环来实现斐波那契数列
+            int f1=0;
+            int f2=1;
+            int f3=0;
+            for(int i=1;i<n;i++) {
+                f3=f1+f2;
+                f1=f2;
+                f2=f3;
+            }
+            System.out.println("F（n）="+f3);
+        }
+    }
 
 
     @Test
